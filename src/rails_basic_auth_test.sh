79a7195d7db4dbba4d2f9b6f72c6c6afe996d575
dave doolin <david.doolin@gmail.com>
2022-01-07 05:14:14 -0800

    Downgrade to ruby 2.7.2
    Ruby 3.1 won't bundle on M1 architecture, sadly. 2.7.2 has
    been out for years and seems to be super stable everywhere.
    Real bummer that I can't use 3.1 yet.

--- src/rails_basic_auth_test.sh
+++ src/rails_basic_auth_test.sh
@@ -3,4 +3,4 @@
 # Must use bash to get `echo -n` to work correctly.
 encoded=$(echo -n "username:password" | base64)
 # curl -X GET -H "Authorization: Basic $encoded" http://localhost:9292/
-curl -X GET -H "Authorization: Basic            $encoded" http://localhost:9292/
+curl -X GET -H "Authorization: Basic $encoded" http://localhost:9292/
