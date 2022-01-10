a8f0bdf39dcae8ea4d7548c45c566ac8304c0038
dave doolin <david.doolin@gmail.com>
2022-01-10 04:20:07 -0800

    added an unauthorized example

--- src/rails_basic_auth_test.sh
+++ src/rails_basic_auth_test.sh
@@ -4,3 +4,8 @@
 encoded=$(echo -n "username:password" | base64)
 # curl -X GET -H "Authorization: Basic $encoded" http://localhost:9292/
 curl -X GET -H "Authorization: Basic $encoded" http://localhost:9292/
+
+echo ""
+echo "Unauthorized"
+encoded=$(echo -n "username1:password1" | base64)
+curl -v -X GET -H "Authorization: Basic $encoded" http://localhost:9292/
