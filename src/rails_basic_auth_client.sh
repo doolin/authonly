335d412e307b0bcba326ba5f2b0af724d9fb5722
dave doolin <david.doolin@gmail.com>
2022-12-30 15:46:56 -0800

    rename the basic auth example

--- src/rails_basic_auth_client.sh
+++ src/rails_basic_auth_client.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# This simple client makes an authorized request first,
-# then a second unauthorized request.
+# TODO: try an invalid login
 
-# Authorized request.
 # Must use bash to get `echo -n` to work correctly.
 encoded=$(echo -n "username:password" | base64)
 # curl -X GET -H "Authorization: Basic $encoded" http://localhost:9292/
