672c08052165e68a38efdf3f87eaf1b84bbd2c61
dave doolin <david.doolin@gmail.com>
2022-12-31 11:58:51 -0800

    signing jwt cleaned up

--- src/jwt_signing.rb
+++ src/jwt_signing.rb
@@ -7,7 +7,7 @@
 
 # TODO: what needs to happen
 # - Create a public/private key pair
-# - Save the key pair to a local fi;e
+# - Save the key pair to a local file
 # - Read the key files in as necessary for signing.
 # - Write some sort of spec which can verify the signing works,
 
@@ -74,12 +74,12 @@ class JwtDemoToken
     # OpenSSL::PKey::RSA.new('foobar', nil)
   end
 
-  def header
-    {
-      'alg' => 'RS256',
-      'typ' => 'JWT'
-    }.to_json
-  end
+  # def header
+  #   {
+  #     'alg' => 'RS256',
+  #     'typ' => 'JWT'
+  #   }.to_json
+  # end
 
   def payload
     {
@@ -93,37 +93,52 @@ RSpec.describe self do # rubocop:disable Metrics/BlockLength
   before do
     RsaKey.create_and_write_rsa_pem_keys
     RsaKey.set_envvars
-    # puts ENV.fetch('JWT_DEMO_PUBLIC_KEY', nil)
-    # puts ENV.fetch('JWT_DEMO_PRIVATE_KEY', nil)
   end
 
-  example do
-    token = JwtDemoToken.new.create
-    _json = JwtDemoToken.decode(token)
-    # binding.irb
-    expect(JwtDemoToken.new).not_to be nil
+  context 'with corrent RSA key' do
+    it 'verifies correctly' do
+      token = JwtDemoToken.new.create
+
+      expect do
+        JwtDemoToken.decode(token)
+      end.not_to raise_error
+    end
+
+    example 'parses payload and header' do
+      token = JwtDemoToken.new.create
+      payload, header = *JwtDemoToken.decode(token)
+      payload = JSON.parse(payload)
+
+      expected_payload = {
+        'loggedInAs' => 'admin',
+        'iat' => 1_422_779_638
+      }
+      expect(payload).to eq expected_payload
+
+      expected_header = { 'alg' => 'RS256' }
+      expect(header).to eq expected_header
+    end
   end
 
-  example 'bogus public key' do
-    # This works as we're generating a new key every time we run the test.
-    bogus_key = <<~KEY
-      -----BEGIN PUBLIC KEY-----
-      MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs7tqIh8t7/Apq4bMwMS+
-      itZY/AJuKpTC4fDwLlDh5ESFTV+wJXAq2cydnK1PtgPOPMpekqtT4AygHuO6xWEO
-      e9hMfcyye7R4g+/WUgem7iOQ3Y/kS8e0fmpw5cAQo8qAogd2ZzatmySIxyB1Zdzp
-      8hqyd+K1f2WW5DiQyIomldlm1haVB2ZxC85yTVETiDMVCR+g3Y+poUIUO/KiTBDZ
-      qbzhabcRYv5zKi8EkBwVAE+yyt79FmZOnD1VWuYpw6oH0hQJVtB441P2cFHpDpz2
-      3VUJ7NynJiWoWAlXRIeOo4cl2pjb9jAwEBhxeXclIfcUDPNU+DcAd+ecUvaYZk+K
-      UwIDAQAB
-      -----END PUBLIC KEY-----
-    KEY
-    ENV['JWT_DEMO_PUBLIC_KEY'] = bogus_key
+  context 'woth incorrect RSA key' do
+    example 'bogus public key' do
+      # This is bogus as we're generating a new key every time we run the test.
+      ENV['JWT_DEMO_PUBLIC_KEY'] = <<~KEY
+        -----BEGIN PUBLIC KEY-----
+        MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs7tqIh8t7/Apq4bMwMS+
+        itZY/AJuKpTC4fDwLlDh5ESFTV+wJXAq2cydnK1PtgPOPMpekqtT4AygHuO6xWEO
+        e9hMfcyye7R4g+/WUgem7iOQ3Y/kS8e0fmpw5cAQo8qAogd2ZzatmySIxyB1Zdzp
+        8hqyd+K1f2WW5DiQyIomldlm1haVB2ZxC85yTVETiDMVCR+g3Y+poUIUO/KiTBDZ
+        qbzhabcRYv5zKi8EkBwVAE+yyt79FmZOnD1VWuYpw6oH0hQJVtB441P2cFHpDpz2
+        3VUJ7NynJiWoWAlXRIeOo4cl2pjb9jAwEBhxeXclIfcUDPNU+DcAd+ecUvaYZk+K
+        UwIDAQAB
+        -----END PUBLIC KEY-----
+      KEY
 
-    # puts bogus_key
-
-    token = JwtDemoToken.new.create
-    expect do
-      JwtDemoToken.decode(token)
-    end.to raise_error(JWT::VerificationError)
+      token = JwtDemoToken.new.create
+      expect do
+        JwtDemoToken.decode(token)
+      end.to raise_error(JWT::VerificationError)
+    end
   end
 end
