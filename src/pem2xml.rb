d5bb48ba7b52c7e514d4526da67edce8fd7c466a
dave doolin <david.doolin@gmail.com>
2023-01-22 11:07:58 -0800

    stub for signed url code

--- src/pem2xml.rb
+++ src/pem2xml.rb
@@ -9,31 +9,38 @@ require 'byebug'
 # https://ruby-doc.org/stdlib-3.1.0/libdoc/openssl/rdoc/OpenSSL/PKey.html
 # Go the other way: https://groups.google.com/g/mailing.openssl.users/c/8hAgyv4SiFY
 
-def base64(bn) # rubocop:disable Naming/MethodParameterName
-  [bn.to_s(2)].pack('m0')
-end
+# Create an MS RSA struct
+class Pem2Xml
+  def initialize(file_path)
+    @file_path = file_path
+  end
 
-# key = OpenSSL::PKey::RSA.new(File.read($stdin))
-# key = OpenSSL::PKey::RSA.new(File.read('../../.ssh/cqm.pem'))
-key = OpenSSL::PKey::RSA.new(2048)
+  def rsa_xml
+    # key = OpenSSL::PKey::RSA.new(File.read($stdin))
+    # key = OpenSSL::PKey::RSA.new(File.read('../../.ssh/cqm.pem'))
 
-# byebug
+    key = OpenSSL::PKey::RSA.new(2048)
 
-key = key.public_key
+    key = key.public_key
 
-key = OpenSSL::PKey::RSA.new($stdin)
-builder = Nokogiri::XML::Builder.new do |xml|
-  xml.RSAKeyValue do
-    xml.Modulus base64(key.n)
-    xml.Exponent base64(key.e)
-    if key.private?
-      xml.D base64(key.d)
-      xml.P base64(key.p)
-      xml.Q base64(key.q)
-      xml.DP base64(key.dmp1)
-      xml.DQ base64(key.dmq1)
-      xml.InverseQ base64(key.iqmp)
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.RSAKeyValue do
+        xml.Modulus base64(key.n)
+        xml.Exponent base64(key.e)
+        if key.private?
+          xml.D base64(key.d)
+          xml.P base64(key.p)
+          xml.Q base64(key.q)
+          xml.DP base64(key.dmp1)
+          xml.DQ base64(key.dmq1)
+          xml.InverseQ base64(key.iqmp)
+        end
+      end
     end
+    puts builder.to_xml
+  end
+
+  def base64(bn) # rubocop:disable Naming/MethodParameterName
+    [bn.to_s(2)].pack('m0')
   end
 end
-puts builder.to_xml
---
+++ src/signed_urls.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+# Here's something to do at some point:
+# https://kukicola.io/posts/signed-urls-with-ruby/
+
