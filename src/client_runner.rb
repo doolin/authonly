b7a46c15a9d8270d17bf87a0e8f96ba17ea4e412
dave doolin <david.doolin@gmail.com>
2024-07-15 04:56:38 -0700

    client runner hooked up to sh script

--- src/client_runner.rb
+++ src/client_runner.rb
@@ -4,9 +4,9 @@
 require 'colorize'
 require 'cli/ui'
 
-
 def rack_basic_auth(_args)
   puts 'Rack basic auth'
+  `./rack_basic_auth_client.sh`
 end
 
 def jwt(_args)
@@ -14,10 +14,7 @@ def jwt(_args)
 end
 
 CLI::UI::Prompt.instructions_color = CLI::UI::Color::GRAY
-
-# rubocop:disable Performance/MethodObjectAsBlock
 CLI::UI::Prompt.ask('Which server?') do |handler|
-  handler.option('Rack basic auth', &method(:rack_basic_auth)) 
+  handler.option('Rack basic auth', &method(:rack_basic_auth))
   handler.option('JWT', &method(:jwt))
 end
-# rubocop:enable Performance/MethodObjectAsBlock
