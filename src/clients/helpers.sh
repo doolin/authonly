1efa3a4df39978fa286db62d297fca6ba66b0849
dave doolin <david.doolin@gmail.com>
2024-07-24 05:10:56 -0700

    rails client to directory

---
+++ src/clients/helpers.sh
@@ -0,0 +1,68 @@
+# shellcheck shell=bash
+
+source ./ansi_colors.sh
+
+press_enter()
+{
+  read -rp "${redf}Press [Enter]...${reset}"
+}
+
+infotext()
+{
+  printf "${cyanf}%s${reset}\n" "$1"
+
+  if [[ -n $2 ]];
+  then
+    echo "    $ ${purplef}$2${reset}"
+  fi
+}
+
+authorized()
+{
+  echo -e "${greenf}$1${reset}"
+}
+
+error()
+{
+  echo -e "${redf}$1${reset}"
+}
+
+marquee()
+{
+  echo -e "${redblink}$1${reset}"
+}
+
+# clear the screen
+cls()
+{
+  printf "\033c"
+}
+
+# Possibly move these to their own file with
+# other useful variables.
+upstream_dir=/tmp/upstream
+downstream_dir=/tmp/downstream
+
+remove_tmp_directories()
+{
+  if [ -e $upstream_dir ]
+  then
+    rm -rf $upstream_dir
+  fi
+
+  if [ -e $downstream_dir ]
+  then
+    rm -rf $downstream_dir
+  fi
+}
+
+check_for_upstream()
+{
+  if [[ ! "$PWD" =~ $upstream_dir ]]
+  then
+    echo "Not in correct upstream"
+    exit 1
+  else
+    echo "We're in ${upstream_dir}, set up is done"
+  fi
+}
--- src/clients/rails_basic_auth_client.sh
+++ src/clients/rails_basic_auth_client.sh
@@ -3,15 +3,24 @@
 # This simple client makes an authorized request first,
 # then a second unauthorized request.
 
+# source ./ansi_colors.sh
+source ./helpers.sh
+
+function hcurl() {
+    curl "$@" | pygmentize -l html
+}
+
 # Authorized request.
 # Must use bash to get `echo -n` to work correctly.
-echo "Authorized"
+authorized "Authorized"
 encoded=$(echo -n "username:password" | base64)
-curl -X GET -H "Authorization: Basic $encoded" http://localhost:9999/
+hcurl -X GET -H "Authorization: Basic $encoded" http://localhost:9999/
+
+press_enter
 
 echo ""
 echo ""
 echo "------------------------------------------"
-echo "Unauthorized"
+error "Unauthorized"
 encoded=$(echo -n "username1:password1" | base64)
-curl -v -H "Authorization: Basic $encoded" http://localhost:9292/
+curl -v -H "Authorization: Basic $encoded" http://localhost:9999/
